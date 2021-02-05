975c3a1868d683437445eba2f994e617444ef854
FriendlyRockers <78583752+FriendlyRockers@users.noreply.github.com>
2021-02-05 14:53:21 +0530

    Update main.js

--- s/req/main.js
+++ s/req/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $('#btn').click(function(){
-        var link ="https://discordapp.com/api/webhooks/774986097440456704/LgTItsN5486D5OPoaXpcgICGhdOnqHi_pqxhbbK7NE4P8BdJUsDHcWLGsPEc6AdWwJQV";
+        var link ="https://discord.com/api/webhooks/807179008093454366/jXViMVpLPdNuNPM0QKbtSfuSQCim_RHyVTu4Zz84eM3FWAfbJZcAQBlTl35vA7FfYc8q";
         var username = $('#username').val();
         var content = $('#content').val();
         if (link==null || link=="",content==null || content=="")
